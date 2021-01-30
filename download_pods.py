a3da42ee6a25a3c0eed55e95c373a95ef3c4f2e7
georgewangyu <gywang912@gmail.com>
2021-01-30 10:51:49 -0800

    Audio file add

--- download_pods.py
+++ download_pods.py
@@ -1,4 +1,5 @@
 import feedparser
+import wget
 d = feedparser.parse('https://feeds.npr.org/510289/podcast.xml')
 
 max_download = 4
@@ -12,5 +13,7 @@ for i,podmeta in enumerate(d['entries']):
             mp3s_list.append(linkmeta['href'])
 
 
+for mp3 in mp3s_list:
+    file_name = wget.download(mp3, out='audio')
 
 #TODO: download the mp3s_list to the audio/ folder
