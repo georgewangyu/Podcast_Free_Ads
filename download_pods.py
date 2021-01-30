0aa349e957b81c129f9022df792450aaaa74eeeb
Daniel Korchinski <danielkorchinski@gmail.com>
2021-01-30 10:25:10 -0800

    added stripping of planety money podcast rss feed

--- download_pods.py
+++ download_pods.py
@@ -1,3 +1,19 @@
 import feedparser
 d = feedparser.parse('https://feeds.npr.org/510289/podcast.xml')
-d['npr']['Planet Money']
+# d['npr']['Planet Money']
+
+d['entries'][0]
+max_download = 4
+
+mp3s_list = []
+
+for i,podmeta in enumerate(d['entries']):
+    if(i > max_download):
+        break
+    for linkmeta in podmeta['links']:
+        if('.mp3' in linkmeta['href']):
+            mpi3s_list.append(linkmeta['href'])
+
+
+
+#TODO: download the mp3s_list to the audio/ folder
