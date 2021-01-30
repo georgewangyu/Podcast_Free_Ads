82d30c4bba8c54d72afd4aadbc6f1e1853e6d47e
Daniel Korchinski <danielkorchinski@gmail.com>
2021-01-30 12:24:14 -0800

    Updated audio-manager to load files, run default test, etc

--- audio_manager.py
+++ audio_manager.py
@@ -1,5 +1,29 @@
-import dejavu
+from pydub import AudioSegment
+import numpy as np 
+
+
+def load_to_raw(filename : str):
+    audiofile = AudioSegment.from_file(filename)
+    rawdata = np.fromstring(audiofile.raw_data, np.int16)
+
+    channels = []
+    for chn in range(audiofile.channels):
+        channels.append(rawdata[chn::audiofile.channels])
+
+    return channels,audiofile
+
+
+def fingerprint(rawdata,rate : float):
+
+    pass 
+
+
+# def strip():
+#     pass 
 
 
 
-
+if(__name__ == '__main__'):
+    source = 'audio/20210129_pmoney_pmpod1060_2.mp3'
+    channels,audiofile = load_to_raw(source) 
+    fingerprint(channels,audiofile.frame_rate) 
